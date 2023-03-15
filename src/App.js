9e2ed21ffc4ffab8f38e260da39e4e4d8e123df5
akshay <kumarakshay6979@gmail.com>
2023-03-15 18:15:15 +0530

    used routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import Home from './Pages/Home';
+import { Routes, Route } from 'react-router-dom';
+import NotFound from './Pages/NotFound';
+import Starred from './Pages/Starred';
 function App() {
   return (
-    <div>
-      hi
-    </div>
+    <Routes>
+      <Route exact path="/" element={<Home />}></Route>
+      <Route exact path="/starred" element={<Starred />}></Route>
+      <Route path="*" element={<NotFound />}>
+        not found
+      </Route>
+    </Routes>
   );
 }
 
